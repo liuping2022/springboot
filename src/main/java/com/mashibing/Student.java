fcbbb422a7d01510c7d09435043db3ae0518b6af
liuping <1045017527@qq.com>
2022-03-29 22:57:01 +0800

    更新

---
+++ src/main/java/com/mashibing/Student.java
@@ -0,0 +1,11 @@
+package com.mashibing;
+
+/**
+ * @author : liuping
+ * @date: 2022/3/29 - 03 - 29 - 22:54
+ * @Description: com.mashibing
+ * @version: 1.0
+ */
+public class Student {
+    private String name;
+}
