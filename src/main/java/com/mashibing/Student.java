b1ff6bcf9849f0fef53b717e1d16c64671d29a8c
liuping <1045017527@qq.com>
2022-03-30 09:43:47 +0800

    更新

--- src/main/java/com/mashibing/Student.java
+++ src/main/java/com/mashibing/Student.java
@@ -10,4 +10,37 @@ public class Student {
     private String name;
     private Integer age;
     private String sex;
+    private Integer height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
 }
